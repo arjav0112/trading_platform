1ea839f0365cd0f32c14a96b14cb9194b4caea2e
Arjav Jain <jarjav2005@gmail.com>
2025-02-03 22:20:11 +0530

    bug fix

--- frontend/src/support/support.jsx
+++ frontend/src/support/support.jsx
@@ -1,6 +1,6 @@
 import Footer from "../utils/footer";
 import Navbar from "../utils/navbar";
-import CreateTicket from "./Createticket";
+import CreateTicket from "./CreateTicket";
 import Hero from "./Hero";
 
 export default function Support(){
